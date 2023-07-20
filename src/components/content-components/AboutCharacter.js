a7502d24b502919182d3f4dbd03b761866c17eba
OliverLFJ <oliverde10021@gmail.com>
2023-07-20 12:25:39 -0600

    Individual Character Finished and Design for Mobiles too

--- src/components/content-components/AboutCharacter.js
+++ src/components/content-components/AboutCharacter.js
@@ -2,35 +2,38 @@ import { useDataContext } from "../../contexts/DataContext";
 import PaginatedButtons from "./PaginatedButtons";
 import FiltersCharacters from "./FiltersCharacters";
 import DontFind from "../error-components/DontFind";
+import { useDataIndividualContext } from "../../contexts/DataIndividualContext";
+import { Link } from "react-router-dom";
 
 const AboutCharacter = () => {
     const { characters, dontFind } = useDataContext();
-
+    const { setCharacter } = useDataIndividualContext();
     return (
         <div className="character-general-container">
             <FiltersCharacters />
-
             {dontFind ? <DontFind /> :
                 <div className="container-recents">
                     {characters.map((item, index) => {
                         return (
-                            <div key={index} className="card-recent-character">
-                                <div className="portal-resource">
-                                    <h4>{item.id}</h4>
-                                </div>
-                                <img alt="character" className="character-img" src={item.image} />
-                                <div className="info-character">
-                                    <h3 className="name-character">{item.name}</h3>
-                                    <div className="footer-card">
-                                        <div className="state-character" style={{
-                                            backgroundColor: item.status === "Alive" ? "#97ce4c" : item.status === "Dead" ? "red" : item.status === "unknown" ? "gray" : ""
-                                        }}>
-                                            {item.status}</div>
-                                        <h4>{item.gender}</h4>
-                                        <h4>{item.species}</h4>
+                            <Link key={index} to="/character" onClick={() => setCharacter(item.id)}>
+                                <div key={index} className="card-recent-character">
+                                    <div className="portal-resource">
+                                        <h4>{item.id}</h4>
+                                    </div>
+                                    <img alt="character" className="character-img" src={item.image} />
+                                    <div className="info-character">
+                                        <h3 className="name-character">{item.name}</h3>
+                                        <div className="footer-card">
+                                            <div className="state-character" style={{
+                                                backgroundColor: item.status === "Alive" ? "#97ce4c" : item.status === "Dead" ? "red" : item.status === "unknown" ? "gray" : ""
+                                            }}>
+                                                {item.status}</div>
+                                            <h4>{item.gender}</h4>
+                                            <h4>{item.species}</h4>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
+                            </Link>
                         )
                     })}
                 </div>
--- src/components/individual-character/IndividualCharacter.js
+++ src/components/individual-character/IndividualCharacter.js
@@ -7,34 +7,38 @@ const IndividualCharacter = () => {
     if (Object.keys(characterInformation).length > 0) {
         return (
             <div className="individual-character-container">
-                <div>
-                    <h2>{characterInformation.name}</h2>
+                <div className="title-container-card">
+                    <h2 className="title-character">{characterInformation.name}</h2>
                 </div>
-                <div>
-                    <img src={characterInformation.image} alt="Character" />
+                <div className="image-character-container">
+                    <img className="image-character" src={characterInformation.image} alt="Character" />
                 </div>
-                <div>
-                    <div>
-                        <h4>Specie</h4>
-                        <p>{characterInformation.species}</p>
+                <div className="information-container-card">
+                    <div className="container-information">
+                        <div className="info-container">
+                            <h4 className="information-card-title">Specie</h4>
+                            <p className="card-information-information">{characterInformation.species}</p>
+                        </div>
+                        <div className="info-container">
+                            <h4 className="information-card-title">Status</h4>
+                            <p className="card-information-information status-card" style={{
+                                backgroundColor: characterInformation.status === "Alive" ? "#97ce4c" : characterInformation.status === "Dead" ? "red" : characterInformation.status === "unknown" ? "gray" : ""
+                            }}>{characterInformation.status}</p>
+                        </div>
+                        <div className="info-container">
+                            <h4 className="information-card-title">Gender</h4>
+                            <p className="card-information-information">{characterInformation.gender}</p>
+                        </div>
                     </div>
-                    <div>
-                        <h4>Status</h4>
-                        <p>{characterInformation.status}</p>
-                    </div>
-                    <div>
-                        <h4>Gender</h4>
-                        <p>{characterInformation.gender}</p>
-                    </div>
-                </div>
-                <div>
-                    <div>
-                        <h4>Origin</h4>
-                        <p>{characterInformation.origin.name}</p>
-                    </div>
-                    <div>
-                        <h4>Location</h4>
-                        <p>{characterInformation.location.name}</p>
+                    <div className="container-information-second">
+                        <div className="info-container">
+                            <h4 className="information-card-title">Origin</h4>
+                            <p className="card-information-information">{characterInformation.origin.name}</p>
+                        </div>
+                        <div className="info-container">
+                            <h4 className="information-card-title">Location</h4>
+                            <p className="card-information-information">{characterInformation.location.name}</p>
+                        </div>
                     </div>
                 </div>
             </div>
